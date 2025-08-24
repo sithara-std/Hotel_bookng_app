ca705059287fb2b99891c99ee0711d79bdb5ffbb
sithara sewwandi <bssitharasewwandi.00@gmail.com>
2025-08-24 22:14:53 +0530

    bottom navigation bar

---
+++ lib/home_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_booking_app/Screens/booking_screen.dart';
+import 'package:hotel_booking_app/Screens/discover_screen.dart';
+import 'package:hotel_booking_app/Screens/favourite_screen.dart';
+import 'package:hotel_booking_app/Screens/message_screen.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  int screenNo = 0;
+  
+  List<Widget> screenList = const [
+    DiscoverScreen(),
+    FavouriteScreen(),
+    BookingScreen(),
+    MessageScreen(),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screenList[screenNo],
+      
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: const Color.fromARGB(202, 0, 0, 0)
+          ),
+          width: double.infinity,
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              bottomNavigationIcon(
+                icon:Icons.favorite,iconText: "Discover",index: 0),
+              bottomNavigationIcon(
+                icon:Icons.favorite,iconText: "Favourite",index: 1),
+              bottomNavigationIcon(
+                icon:Icons.shopping_bag,iconText: "Booking",index: 2),
+              bottomNavigationIcon(
+                icon:Icons.ac_unit_outlined,iconText: "Message",index: 3),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget bottomNavigationIcon(
+    {required IconData icon,required String iconText,required int index}){
+      return InkWell(
+        onTap: (){
+          setState(() {
+            screenNo = index;
+          });
+          print(screenNo);
+        },
+        child: Column(
+          children: [
+            Icon(
+              icon,
+              color: screenNo == index? Colors.white:Colors.grey,
+              size: screenNo  == index ? 23 : 18,
+            ),
+            Text(
+              iconText,
+              style: TextStyle(
+                color: screenNo == index ? Colors.white: Colors.grey
+              ),
+            )
+          ],
+        ),
+      );
+    }
+}
